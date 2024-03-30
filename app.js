e93e8a199bbd51267f6b903ba74cb632073c44c2
Xtrullor73 <xtrullo@bk.ru>
2024-03-30 14:06:53 -0700

    style: Refactor code to comply with ESLint

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('GeoTasker API Running')
+  res.send('GeoTasker API Running')
 })
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
+  console.log(`Running on port ${PORT}`)
 })
